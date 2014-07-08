0e5244b1eeb39308f4d63d31b9c24524b3e5bfd0
remeh <remeh@remeh.fr>
2014-07-08 14:11:31 +0200

    Generate a ID with a minimum amount of digits.

--- goid.go
+++ goid.go
@@ -1,29 +1,50 @@
 package goid
 
+import (
+	"math"
+)
+
 // Generates a pseudo-random (look alike) ID
 // by using another base to represent it.
 //
-// The ID is generated within the given values and
+// The ID is generated within the given digits and
 // the base is deducted from the size of the array.
 //
 // The seed is the number of already generated IDs.
-func GenerateNext(values []byte, seed int) string {
-	if seed == 0 {
-		return "0"
+//
+// The minDigitCount is how many digits minimum should
+// have the generated ID.
+//
+// This method doesn't return an error but can return an empty string
+// if the given array of digits is empty.
+func GenerateNext(digits []byte, seed int, minDigitCount int) string {
+	if len(digits) == 0 {
+		return "";
 	}
-
-	if len(values) == 0 {
-		return ""
-	}
-
-	// Guess which base we want to target
-	base := len(values)
 
 	result := make([]byte, 0)
 
-	for i := seed; i%base > 0 || i/base > 0; i /= base {
-		result = append(result, values[i%base])
+	// Special case for the 0 
+	if seed == 0 {
+		result = append(result, '0');
+		for i := 0; i < minDigitCount-1; i++ {
+			result = append(result, '0');
+		}
+	} else {
+		// Guess which base we want to target
+		base := len(digits)
+
+		// offset to have a minimum amount of digits
+		if minDigitCount > 1 {
+			seed += int(math.Pow(float64(base), float64(minDigitCount-1)))
+		}
+
+
+		for i := seed; i%base > 0 || i/base > 0; i /= base {
+			result = append(result, digits[i%base])
+		}
 	}
+
 
 	return string(result)
 }
--- goid_test.go
+++ goid_test.go
@@ -1,6 +1,8 @@
 package goid
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSimpleGenerate(t *testing.T) {
 	values := make([]byte, 4)
@@ -10,74 +12,96 @@ func TestSimpleGenerate(t *testing.T) {
 	values[3] = 'A'
 
 	for i := 0; i < 4; i++ {
-		if GenerateNext(values, 0) != "0" {
-			t.Fatalf("Got %s instead of 1", GenerateNext(values, 0))
+		if GenerateNext(values, 0, 0) != "0" {
+			t.Fatalf("Got %s instead of 1", GenerateNext(values, 0, 0))
 		}
-		if GenerateNext(values, 1) != "1" {
-			t.Fatalf("Got %s instead of 1", GenerateNext(values, 1))
+		if GenerateNext(values, 1, 0) != "1" {
+			t.Fatalf("Got %s instead of 1", GenerateNext(values, 1, 0))
 		}
-		if GenerateNext(values, 2) != "2" {
-			t.Fatalf("Got %s instead of 1", GenerateNext(values, 2))
+		if GenerateNext(values, 2, 0) != "2" {
+			t.Fatalf("Got %s instead of 1", GenerateNext(values, 2, 0))
 		}
-		if GenerateNext(values, 3) != "A" {
-			t.Fatalf("Got %s instead of A", GenerateNext(values, 3))
+		if GenerateNext(values, 3, 0) != "A" {
+			t.Fatalf("Got %s instead of A", GenerateNext(values, 3, 0))
 		}
-		if GenerateNext(values, 4) != "01" {
-			t.Fatalf("Got %s instead of 01", GenerateNext(values, 4))
+		if GenerateNext(values, 4, 0) != "01" {
+			t.Fatalf("Got %s instead of 01", GenerateNext(values, 4, 0))
 		}
-		if GenerateNext(values, 5) != "11" {
-			t.Fatalf("Got %s instead of 11", GenerateNext(values, 5))
+		if GenerateNext(values, 5, 0) != "11" {
+			t.Fatalf("Got %s instead of 11", GenerateNext(values, 5, 0))
 		}
-		if GenerateNext(values, 6) != "21" {
-			t.Fatalf("Got %s instead of 21", GenerateNext(values, 6))
+		if GenerateNext(values, 6, 0) != "21" {
+			t.Fatalf("Got %s instead of 21", GenerateNext(values, 6, 0))
 		}
-		if GenerateNext(values, 7) != "A1" {
-			t.Fatalf("Got %s instead of A1", GenerateNext(values, 7))
+		if GenerateNext(values, 7, 0) != "A1" {
+			t.Fatalf("Got %s instead of A1", GenerateNext(values, 7, 0))
 		}
-		if GenerateNext(values, 8) != "02" {
-			t.Fatalf("Got %s instead of 02", GenerateNext(values, 8))
+		if GenerateNext(values, 8, 0) != "02" {
+			t.Fatalf("Got %s instead of 02", GenerateNext(values, 8, 0))
 		}
-		if GenerateNext(values, 9) != "12" {
-			t.Fatalf("Got %s instead of 12", GenerateNext(values, 9))
+		if GenerateNext(values, 9, 0) != "12" {
+			t.Fatalf("Got %s instead of 12", GenerateNext(values, 9, 0))
 		}
-		if GenerateNext(values, 10) != "22" {
-			t.Fatalf("Got %s instead of 22", GenerateNext(values, 10))
+		if GenerateNext(values, 10, 0) != "22" {
+			t.Fatalf("Got %s instead of 22", GenerateNext(values, 10, 0))
 		}
-		if GenerateNext(values, 11) != "A2" {
-			t.Fatalf("Got %s instead of A2", GenerateNext(values, 11))
+		if GenerateNext(values, 11, 0) != "A2" {
+			t.Fatalf("Got %s instead of A2", GenerateNext(values, 11, 0))
 		}
-		if GenerateNext(values, 12) != "0A" {
-			t.Fatalf("Got %s instead of 0A", GenerateNext(values, 12))
+		if GenerateNext(values, 12, 0) != "0A" {
+			t.Fatalf("Got %s instead of 0A", GenerateNext(values, 12, 0))
 		}
-		if GenerateNext(values, 13) != "1A" {
-			t.Fatalf("Got %s instead of 1A", GenerateNext(values, 13))
+		if GenerateNext(values, 13, 0) != "1A" {
+			t.Fatalf("Got %s instead of 1A", GenerateNext(values, 13, 0))
 		}
-		if GenerateNext(values, 14) != "2A" {
-			t.Fatalf("Got %s instead of 2A", GenerateNext(values, 14))
+		if GenerateNext(values, 14, 0) != "2A" {
+			t.Fatalf("Got %s instead of 2A", GenerateNext(values, 14, 0))
 		}
-		if GenerateNext(values, 15) != "AA" {
-			t.Fatalf("Got %s instead of AA", GenerateNext(values, 15))
+		if GenerateNext(values, 15, 0) != "AA" {
+			t.Fatalf("Got %s instead of AA", GenerateNext(values, 15, 0))
 		}
-		if GenerateNext(values, 16) != "001" {
-			t.Fatalf("Got %s instead of 001", GenerateNext(values, 16))
+		if GenerateNext(values, 16, 0) != "001" {
+			t.Fatalf("Got %s instead of 001", GenerateNext(values, 16, 0))
 		}
-		if GenerateNext(values, 17) != "101" {
-			t.Fatalf("Got %s instead of A3", GenerateNext(values, 17))
+		if GenerateNext(values, 17, 0) != "101" {
+			t.Fatalf("Got %s instead of A3", GenerateNext(values, 17, 0))
 		}
-		if GenerateNext(values, 18) != "201" {
-			t.Fatalf("Got %s instead of 201", GenerateNext(values, 18))
+		if GenerateNext(values, 18, 0) != "201" {
+			t.Fatalf("Got %s instead of 201", GenerateNext(values, 18, 0))
 		}
-		if GenerateNext(values, 19) != "A01" {
-			t.Fatalf("Got %s instead of A01", GenerateNext(values, 19))
+		if GenerateNext(values, 19, 0) != "A01" {
+			t.Fatalf("Got %s instead of A01", GenerateNext(values, 19, 0))
 		}
-		if GenerateNext(values, 20) != "011" {
-			t.Fatalf("Got %s instead of 011", GenerateNext(values, 20))
+		if GenerateNext(values, 20, 0) != "011" {
+			t.Fatalf("Got %s instead of 011", GenerateNext(values, 20, 0))
 		}
-		if GenerateNext(values, 21) != "111" {
-			t.Fatalf("Got %s instead of 111", GenerateNext(values, 21))
+		if GenerateNext(values, 21, 0) != "111" {
+			t.Fatalf("Got %s instead of 111", GenerateNext(values, 21, 0))
 		}
-		if GenerateNext(values, 22) != "211" {
-			t.Fatalf("Got %s instead of 211", GenerateNext(values, 22))
+		if GenerateNext(values, 22, 0) != "211" {
+			t.Fatalf("Got %s instead of 211", GenerateNext(values, 22, 0))
 		}
+	}
+
+	g := GenerateNext(values, 5, 4);
+	if len(g) < 4 {
+		t.Fatalf("Got %s instead something which should have 4 digits", g)
+	}
+	g = GenerateNext(values, 55, 4);
+	if len(g) < 4 {
+		t.Fatalf("Got %s instead something which should have 4 digits", g)
+	}
+	g = GenerateNext(values, 43432423435, 4);
+	if len(g) < 4 {
+		t.Fatalf("Got %s instead something which should have 4 digits", g)
+	}
+	g = GenerateNext(values, 5, 12);
+	if len(g) < 12 {
+		t.Fatalf("Got %s instead something which should have 12 digits", g)
+	}
+
+	g = GenerateNext(values, 0, 4);
+	if g != "0000" {
+		t.Fatalf("Should generate 0000 and not : %s", g);
 	}
 }
